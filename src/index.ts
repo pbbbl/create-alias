c488968985707e35c4431e04a0e74cc79c264bb4
Tyler Grow <tyler@pbbbl.com>
2023-04-07 21:15:19 -0600

    publishing

--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,85 @@
 import * as fs from 'fs';
 import * as path from 'path';
-
+import * as fse from 'fs-extra';
 
 export type Alias = Record<string, string>;
 export interface CreateAliasOptions {
-
-    root?: string;
-    baseUrl?: string;
-    jsconfig?: string | string[];
-    tsconfig?: string | string[];
+  root?: string;
+  baseUrl?: string;
+  jsconfig?: string | string[];
+  tsconfig?: string | string[];
 }
 
 function createAlias(
-    alias: Alias,
-    createAliasOptions: CreateAliasOptions = {
-        root: '.',
-        baseUrl: '.',
-        jsconfig: undefined,
-        tsconfig: undefined,
-    }): Alias {
-    const { root, baseUrl, jsconfig, tsconfig }: CreateAliasOptions = {
-        root: '.',
-        baseUrl: '.',
-        jsconfig: undefined,
-        tsconfig: undefined,
-        ...createAliasOptions,
+  alias: Alias,
+  createAliasOptions: CreateAliasOptions = {
+    root: '.',
+    baseUrl: '.',
+    jsconfig: undefined,
+    tsconfig: undefined,
+  },
+): Alias {
+  const { root, baseUrl, jsconfig, tsconfig }: CreateAliasOptions = {
+    root: '.',
+    baseUrl: '.',
+    jsconfig: undefined,
+    tsconfig: undefined,
+    ...createAliasOptions,
+  };
+  const paths: Record<string, string[]> = {};
+  Object.keys(alias).forEach((key) => {
+    const aliasPath = alias[key];
+    paths[`${key}/*`] = [`${aliasPath}/*`];
+  });
+  const jsconfigContent = {
+    compilerOptions: {
+      baseUrl,
+      paths,
+    },
+  };
+  if (jsconfig) {
+    if (Array.isArray(jsconfig)) {
+      jsconfig.forEach((configPath) => {
+        updateOrCreateConfigFile(path.resolve(root, configPath), jsconfigContent);
+      });
+    } else {
+      updateOrCreateConfigFile(path.resolve(root, jsconfig), jsconfigContent);
     }
-    const paths: Record<string, string[]> = {};
-    Object.keys(alias).forEach((key) => {
-        const aliasPath = alias[key];
-        paths[`${key}/*`] = [`${aliasPath}/*`];
-    });
-    const jsconfigContent = {
-        compilerOptions: {
-            baseUrl,
-            paths,
-        },
-    };
-    if (jsconfig) {
-        if (Array.isArray(jsconfig)) {
-            jsconfig.forEach((configPath) => {
-                updateOrCreateConfigFile(path.resolve(root, configPath), jsconfigContent);
-            });
-        } else {
-            updateOrCreateConfigFile(path.resolve(root, jsconfig), jsconfigContent);
-        }
+  }
+  const tsconfigContent = {
+    compilerOptions: {
+      baseUrl,
+      paths,
+    },
+  };
+  if (tsconfig) {
+    if (Array.isArray(tsconfig)) {
+      tsconfig.forEach((configPath) => {
+        updateOrCreateConfigFile(path.resolve(root, configPath), tsconfigContent);
+      });
+    } else {
+      updateOrCreateConfigFile(path.resolve(root, tsconfig), tsconfigContent);
     }
-    const tsconfigContent = {
-        compilerOptions: {
-            baseUrl,
-            paths,
-        }
-    };
-    if (tsconfig) {
-        if (Array.isArray(tsconfig)) {
-            tsconfig.forEach((configPath) => {
-                updateOrCreateConfigFile(path.resolve(root, configPath), tsconfigContent);
-            });
-        } else {
-            updateOrCreateConfigFile(path.resolve(root, tsconfig), tsconfigContent);
-        }
-    }
-    return alias;
+  }
+  return alias;
 }
 
 function updateOrCreateConfigFile(configPath: string, content: any) {
-    try {
-        const existingConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
-        const mergedConfig = {
-            ...existingConfig,
-            ...content,
-        };
-        fs.writeFileSync(configPath, JSON.stringify(mergedConfig, null, 2));
-    } catch (error: any) {
-        if (error.code === 'ENOENT') {
-            fs.writeFileSync(configPath, JSON.stringify(content, null, 2));
-        } else {
-            throw error;
-        }
+  fse.ensureDirSync(path.dirname(configPath));
+  try {
+    const existingConfig = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+    const mergedConfig = {
+      ...existingConfig,
+      ...content,
+    };
+    fs.writeFileSync(configPath, JSON.stringify(mergedConfig, null, 2));
+  } catch (error: any) {
+    if (error.code === 'ENOENT') {
+      fs.writeFileSync(configPath, JSON.stringify(content, null, 2));
+    } else {
+      throw error;
     }
+  }
 }
 
 export { createAlias, createAlias as default };
--- src/tests/create-alias.test.ts
+++ src/tests/create-alias.test.ts
@@ -1,103 +1,112 @@
 import { createAlias, CreateAliasOptions, Alias } from '../index';
-import *  as path from 'path';
+import * as path from 'path';
 import * as fs from 'fs';
 
 type TestOptions = {
-    root: string;
-    baseUrl: string;
-    jsconfig?: string | string[];
-    tsconfig?: string | string[];
-}
+  root: string;
+  baseUrl: string;
+  jsconfig?: string | string[];
+  tsconfig?: string | string[];
+};
 const root = path.resolve(__dirname, './test-project');
-const baseUrl = '.'
+const baseUrl = '.';
 
 const alias1: Alias = {
-    '@': 'src',
-    '@components': './src/components',
-    '@hooks': './src/hooks',
-    '@utils': './src/utils',
-}
+  '@': 'src',
+  '@components': './src/components',
+  '@hooks': './src/hooks',
+  '@utils': './src/utils',
+};
 const alias2: Alias = {
-    '#': 'src',
-    '#components': './src/components',
-    '#hooks': './src/hooks',
-    '#utils': './src/utils',
-}
+  '#': 'src',
+  '#components': './src/components',
+  '#hooks': './src/hooks',
+  '#utils': './src/utils',
+};
 
 const expectedFile1 = {
-    compilerOptions: {
-        baseUrl,
-        paths: {
-            '@/*': ['src/*'],
-            '@components/*': ['./src/components/*'],
-            '@hooks/*': ['./src/hooks/*'],
-            '@utils/*': ['./src/utils/*'],
-        }
-    }
-}
+  compilerOptions: {
+    baseUrl,
+    paths: {
+      '@/*': ['src/*'],
+      '@components/*': ['./src/components/*'],
+      '@hooks/*': ['./src/hooks/*'],
+      '@utils/*': ['./src/utils/*'],
+    },
+  },
+};
 const expectedFile2 = {
-    compilerOptions: {
-        baseUrl,
-        paths: {
-            '#/*': ['src/*'],
-            '#components/*': ['./src/components/*'],
-            '#hooks/*': ['./src/hooks/*'],
-            '#utils/*': ['./src/utils/*'],
-        }
-    }
-}
-const tests: Array<TestOptions> = [{
+  compilerOptions: {
+    baseUrl,
+    paths: {
+      '#/*': ['src/*'],
+      '#components/*': ['./src/components/*'],
+      '#hooks/*': ['./src/hooks/*'],
+      '#utils/*': ['./src/utils/*'],
+    },
+  },
+};
+const tests: TestOptions[] = [
+  {
     root,
     baseUrl,
-}, {
+  },
+  {
     root,
     baseUrl,
     jsconfig: `./jsconfig1.json`,
     tsconfig: `./tsconfig1.json`,
-
-}, {
+  },
+  {
     root,
     baseUrl,
     jsconfig: [`./jsconfig2.json`, `./jsconfig3.json`],
     tsconfig: [`./tsconfig2.json`, `./tsconfig3.json`],
-}]
-var cycle = 0;
-const runTest = (options: TestOptions = tests[0], index: number = 0, alias: Alias = alias1, expectedFile: { [key: string]: any }, shouldDeleteFile: boolean = false) => {
-    const prefix = `Alias ${cycle++}`
-    const hasFiles = index > 0;
-    const hasArrayFiles = index === 2;
-    let files: Array<string> = [];
-    if (hasFiles) {
-        if (hasArrayFiles) {
-            files = [...(options.jsconfig as string[]), ...(options.tsconfig as string[])]
-        } else {
-            files = [options.jsconfig as string, options.tsconfig as string]
-        }
+  },
+];
+let cycle = 0;
+const runTest = (
+  options: TestOptions = tests[0],
+  index: number = 0,
+  alias: Alias = alias1,
+  expectedFile: { [key: string]: any },
+  shouldDeleteFile: boolean = false,
+) => {
+  const prefix = `Alias ${cycle++}`;
+  const hasFiles = index > 0;
+  const hasArrayFiles = index === 2;
+  let files: string[] = [];
+  if (hasFiles) {
+    if (hasArrayFiles) {
+      files = [...(options.jsconfig as string[]), ...(options.tsconfig as string[])];
+    } else {
+      files = [options.jsconfig as string, options.tsconfig as string];
     }
-    // test createAlias alias1
-    const created = createAlias(alias, options);
+  }
+  // test createAlias alias1
+  const created = createAlias(alias, options);
 
-    test(`${prefix} - created alias`, () => {
-        expect(created).toEqual(alias)
+  test(`${prefix} - created alias`, () => {
+    expect(created).toEqual(alias);
+  });
+  if (hasFiles) {
+    files.forEach((file) => {
+      const filePath = path.resolve(options.root, file);
+      const json = fs.readFileSync(filePath, 'utf8');
+      const data = JSON.parse(json);
+      test(`${prefix} - ${file} - file data`, () => {
+        expect(data).toEqual(expectedFile);
+      });
+      if (shouldDeleteFile) {
+        fs.unlinkSync(filePath);
+      }
     });
-    if (hasFiles) {
-        files.forEach((file) => {
-            const filePath = path.resolve(options.root, file);
-            const json = fs.readFileSync(filePath, 'utf8');
-            const data = JSON.parse(json);
-            test(`${prefix} - ${file} - file data`, () => {
-                expect(data).toEqual(expectedFile)
-            });
-            if (shouldDeleteFile) {
-                fs.unlinkSync(filePath)
-            }
-        })
-    }
-}
+  }
+};
 tests.forEach((options, index) => {
-    runTest(options, index, alias1, expectedFile1, true);
-})
+  runTest(options, index, alias1, expectedFile1, true);
+});
 
 tests.forEach((options, index) => {
-    runTest(options, index, alias2, expectedFile2, true);
+  runTest(options, index, alias2, expectedFile2, true);
 });
